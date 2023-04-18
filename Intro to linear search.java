6513eafdf7ca4b76ef58596ee6d9857e650daaaa
Vedangi Thokal <117835144+vedangit@users.noreply.github.com>
2023-04-18 17:11:31 +0530

    Create Intro to linear search.java
    Intro to linear search

---
+++ Intro to linear search.java
@@ -0,0 +1,47 @@
+/*Linear search algorithm is the simplest searching algorithm. It simply goes through the list sequentially till your desired target is found.*/
+
+
+
+public class linearsearch {
+    public static void main(String[] args) {
+        int[] arr = {0, 1, 2, 3, 4, 5, 6, 8, 9, 7};
+        int target = 7;
+        int ans = linearsearch(arr, target);
+        System.out.println(ans);
+
+        
+    }
+    //search in the array if item found return index, else return no item found and index as -1
+    static int linearsearch(int[] arr, int target){
+
+        if(arr.length == 0){
+            return -1;
+        }
+
+        // for(int index = 0; index<arr.length; index++){
+        //     int element = arr[index];
+        //     if(element == target){
+        //         return index;
+
+        //     }
+
+        for(int element : arr){
+            if(element == target){
+                return element;
+            }
+        }
+
+        return -1;
+
+    }
+  
+
+}
+
+
+//the output should be 7
+
+
+   
+
+
