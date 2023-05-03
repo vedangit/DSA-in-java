bef0a23d0a5c8f7b317290a010208764b0b87385
Vedangi Thokal <117835144+vedangit@users.noreply.github.com>
2023-05-03 09:35:43 +0530

    Create evennumberofdigits.java
    leetcode question.

---
+++ evennumberofdigits.java
@@ -0,0 +1,52 @@
+/*in this program , we will check how many elements in an array contain an even number of digits */
+
+public class evennumberofdigits {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        int[] nums = {12, 345, 2, 6, 7896};
+        System.out.println(findnumbers(nums));
+    }
+
+//this function will go through the array and count the number of numbers who have even number of digits
+    static int findnumbers(int[] nums) {
+        int count = 0;
+        for(int num : nums) {
+            if(even(num)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+//now we will write a program to check if a certain number has an even number of digits
+	/*ways to do that 
+	1] count the number of digits, the old fashioned way. (n/10 and count++)
+	2] convert the number into a string and do string length
+	*/
+	
+	
+	//first here, we are counting the number of digits in a number
+    static int digits(int num) {
+        int count = 0;
+        //what if the number is negative
+        if(num<0){
+            num = num*-1; //making it positive
+        }
+        if(num ==0){
+            return 1;
+        }
+        while(num > 0) {
+            count++;
+            num = num / 10;
+        }
+        return count;
+    }
+
+    //here, we check if the number of digits are even
+    static boolean even(int num) {
+        int number = digits(num);
+        return number % 2 == 0;
+    }
+}
+
+  
